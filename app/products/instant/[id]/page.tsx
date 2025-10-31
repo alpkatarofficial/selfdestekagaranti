3c8d3667e1b909686c1ddc636d23966037cd60ca
Süleyman Alp Katar <alpkatar@linkdijital.com.tr>
2025-10-31 12:12:59 +0300

    update

--- app/products/instant/[id]/page.tsx
+++ app/products/instant/[id]/page.tsx
@@ -222,7 +222,7 @@ export default async function ProductDetailPage({ params }: ProductDetailPagePro
                   rel="noopener noreferrer"
                   className="inline-block"
                 >
-                  <Button className="w-full bg-red-600 hover:bg-red-700">
+                  <Button className="w-full bg-[#86cbe8] hover:bg-[#5be4cb]">
                     Kullanım Kılavuzunu Görüntüle
                   </Button>
                 </a>
